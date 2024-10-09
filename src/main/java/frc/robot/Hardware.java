8adfef65f7475b2f9e1d4ebcec08d159be8a585c
Owen <117956892+Owen756@users.noreply.github.com>
2024-10-08 18:51:49 -0700

    move hardware to here

--- src/main/java/frc/robot/Hardware.java
+++ src/main/java/frc/robot/Hardware.java
@@ -1,11 +1,22 @@
 package frc.robot;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.config.RobotConfig;
 
 public class Hardware {
+  private RobotConfig CONFIG = RobotConfig.get();
   public final PowerDistribution pdh = new PowerDistribution();
 
   public final CommandXboxController driverController = new CommandXboxController(0);
   public final CommandXboxController operatorController = new CommandXboxController(1);
+
+  public final TalonFX climberMotor = new TalonFX(CONFIG.climber().motorID());
+  public final TalonFX bottomShooter = new TalonFX(CONFIG.shooter().bottomMotorID());
+  public final TalonFX topShooter = new TalonFX(CONFIG.shooter().topMotorID());
+  public final TalonFX intakeMotor = new TalonFX(CONFIG.intake().motorID());
+  public final TalonFX queuerMotor = new TalonFX(CONFIG.queuer().motorID());
+  public final DigitalInput sensor = new DigitalInput(CONFIG.queuer().sensorID());
 }
