4e4694a03cfee8d0c4b67ad1bc548cfa9698cff5
Owen <117956892+Owen756@users.noreply.github.com>
2024-10-29 16:29:22 -0700

    format

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -5,7 +5,7 @@ public enum AutoSelection {
   FOUR_PIECE_SUBWOOFER("4_piece_red_sub", "4_piece_blue_sub"),
   SHOOT_STOP_AMP("shoot_stop_red_amp", "shoot_stop_blue_amp"),
   SHOOT_STOP_SOURCE("shoot_stop_red_source", "shoot_stop_blue_source"),
-  THREE_PIECE_MID_SOURCE("3_piece_red_midline_source","3_piece_blue_midline_source");
+  THREE_PIECE_MID_SOURCE("3_piece_red_midline_source", "3_piece_blue_midline_source");
 
   public final String redAutoName;
   public final String blueAutoName;
