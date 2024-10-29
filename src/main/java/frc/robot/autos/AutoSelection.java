c8ee8236279b81a8e89bfe6358089a9aee42c12c
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2024-10-28 18:36:17 -0700

    add 3 piece race auto

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -4,7 +4,8 @@ public enum AutoSelection {
   DO_NOTHING("", ""),
   FOUR_PIECE_SUBWOOFER("4_piece_red_sub", "4_piece_blue_sub"),
   SHOOT_STOP_AMP("shoot_stop_red_amp", "shoot_stop_blue_amp"),
-  SHOOT_STOP_SOURCE("shoot_stop_red_source", "shoot_stop_blue_source");
+  SHOOT_STOP_SOURCE("shoot_stop_red_source", "shoot_stop_blue_source"),
+  THREE_PIECE_MID_SOURCE("3_piece_red_midline_source","3_piece_blue_midline_source");
 
   public final String redAutoName;
   public final String blueAutoName;
